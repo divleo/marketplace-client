8e88aa3f12f0417025b0437c27110e4675bf1ca1
divleo <11596691+divleo@users.noreply.github.com>
2020-04-28 22:49:00 +0300

    add fetch offers, types and some cosmetic changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,94 @@
-import React from 'react';
-import { Layout, Menu, Breadcrumb } from 'antd';
+import React, { useEffect, useState } from 'react';
+
+import { Layout, Menu, Breadcrumb, Table } from 'antd';
 
 import './App.css';
+
 import logo from './logo.svg';
 
-const { Header, Content, Footer } = Layout;
-const year = new Date().getFullYear();
+type Offer = {
+  key: number;
+  header: string;
+  description: string;
+  price: number;
+};
 
-const App = () => (
-  <div className="container">
+const { Header, Content, Footer } = Layout;
+
+const App = () => {
+  const [offers, setOffers] = useState<Offer[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('http://localhost:5000/offers');
+      const body: any[] = await res.json();
+
+      const offers: Offer[] = body.map((item) => ({
+        key: item.id,
+        header: item.header,
+        description: item.description,
+        price: item.price,
+      }));
+
+      setOffers(offers);
+    };
+
+    fetchData();
+  }, []);
+
+  const columns = [
+    {
+      title: 'Game',
+      dataIndex: 'header',
+      key: 'header',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
+    },
+  ];
+
+  return (
     <Layout className="layout">
       <Header>
-        <div className="logo">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="container">
+          <div className="logo-container">
+            <img src={logo} className="logo-container__logo" alt="logo" />
+          </div>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
         </div>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
       </Header>
+
       <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-content">Content</div>
+        <div className="container">
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-content">
+            <Table dataSource={offers} columns={columns} />
+          </div>
+          {console.log(offers)}
+        </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        Ant Design ©{year} Created by Ant UED
+      <Footer className="footer">
+        <div className="container">
+          <span>© {new Date().getFullYear()} / Marketplace</span>
+        </div>
       </Footer>
     </Layout>
-  </div>
-);
+  );
+};
 
 export default App;
